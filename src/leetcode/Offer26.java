35c15b182ed1def733623966ab2dd2e751709866
mddzzuhust <1539924369@qq.com>
2020-09-24 21:56:22 +0800

    dataStructure

---
+++ src/leetcode/Offer26.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+public class Offer26 {
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        if(B==null||A==null)
+            return false;
+        boolean res = false;
+        if(A.val==B.val){
+            res = isMatch(A,B);
+        }
+        if(!res){
+            res = isSubStructure(A.left,B);
+        }
+        if(!res){
+            res = isSubStructure(A.right,B);
+        }
+        return res;
+    }
+
+    private boolean isMatch(TreeNode a,TreeNode b){
+        if(b==null)
+            return true;
+        if(a==null)
+            return false;
+        if(a.val!=b.val)
+            return false;
+        return isMatch(a.left,b.left)&&isMatch(a.right,b.right);
+    }
+}
