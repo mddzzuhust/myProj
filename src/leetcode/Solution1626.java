359d6b8ef1d3947a550142f5a0f4951500aa9482
mddzzuhust <1539924369@qq.com>
2020-10-24 21:29:56 +0800

    leetcode

---
+++ src/leetcode/Solution1626.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Solution1626 {
+    public int bestTeamScore(int[] scores, int[] ages) {
+        if(scores.length==1)
+            return scores[0];
+        int[][] arr = new int[scores.length][2];
+
+        for (int i = 0; i < scores.length; i++) {
+            arr[i][0] = ages[i];
+            arr[i][1] = scores[i];
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0]==o2[0])
+                    return o1[1]-o2[1];
+                return o1[0]-o2[0];
+            }
+        });
+
+        int[] dp = new int[scores.length];
+        for (int i = 0; i < scores.length; i++) {
+            dp[i] = arr[i][1];
+        }
+
+        int ans = 0;
+        for (int i = 0; i < scores.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if(arr[j][1]<=arr[i][1]){
+                    dp[i]=Math.max(dp[j]+arr[i][1],dp[i]);
+                }
+                ans = Math.max(ans,dp[i]);
+            }
+        }
+        return ans;
+    }
+}
---
+++ src/leetcode/Solution1627.java
@@ -0,0 +1,85 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution1627 {
+    class UnionFind{
+        private int[] parent;
+        private int[] weight;
+        private int size;
+        private int groups;
+
+        public UnionFind(int size){
+            this.parent = new int[size];
+            this.weight = new int[size];
+            this.size = size;
+            this.groups = size;
+
+            for (int i = 0; i < size; i++) {
+                this.parent[i] = i;
+                this.weight[i] = 1;
+            }
+        }
+
+        public int find(int element){
+            while (element!=parent[element]){
+                parent[element]  = parent[parent[element]];
+                element = parent[element];
+            }
+            return element;
+        }
+
+        public boolean isConnected(int firstElement,int secondElement){
+            return find(firstElement) == find(secondElement);
+        }
+
+        public void unionElements(int firstElement,int secondElement){
+            int firstRoot = find(firstElement);
+            int secondRoot = find(secondElement);
+
+            if(firstRoot == secondRoot){
+                return;
+            }
+
+            if(weight[firstRoot]>weight[secondElement]){
+                parent[secondRoot] = firstRoot;
+                weight[firstRoot] += weight[secondRoot];
+            }else {
+                parent[firstRoot] = secondRoot;
+                weight[secondRoot] += weight[firstRoot];
+            }
+
+            this.groups--;
+        }
+
+        public int getGroups(){
+            return this.groups;
+        }
+    }
+
+    public List<Boolean> areConnected(int n,int threshold,int[][] queries){
+        List<Boolean> list = new ArrayList<>();
+        if(threshold==0){
+            for (int i = 0; i < queries.length; i++) {
+                list.add(true);
+            }
+            return list;
+        }
+        UnionFind union = new UnionFind(n);
+        for (int i = threshold+1; i <= n; i++) {
+            int times = 2;
+            while (i*times<=n){
+                union.unionElements(i-1,i*times-1);
+                times += 1;
+            }
+        }
+
+        for (int i = 0; i < queries.length; i++) {
+            int first = queries[i][0];
+            int second = queries[i][1];
+            list.add(union.isConnected(first-1,second-1));
+        }
+        return list;
+    }
+}
