866ae05ccac32d4ad6ba7f9b3c0d68216fb360ca
mddzzuhust <1539924369@qq.com>
2020-11-01 22:14:51 +0800

    leetcode

---
+++ src/leetcode/Solution5555.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class Solution5555 {
+    public int countVowelStrings(int n) {
+        int dp[][] = new int[n + 1][5];
+        for (int i = 0; i < 5; i++) {
+            dp[1][i] = 1;
+        }
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = dp[i - 1][0];
+            dp[i][1] = dp[i - 1][0] + dp[i - 1][1];
+            dp[i][2] = dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2];
+            dp[i][3] = dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][3];
+            dp[i][4] = dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][3] + dp[i - 1][4];
+        }
+        return dp[n][0]+dp[n][1]+dp[n][2]+dp[n][3]+dp[n][4];
+    }
+
+    public static void main(String[] args) {
+        Solution5555 s = new Solution5555();
+        System.out.println(s.countVowelStrings(55));
+    }
+}
