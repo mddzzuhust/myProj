1890b246131e4b955192cab5d4c57a6965f1f609
mddzzuhust <1539924369@qq.com>
2020-12-02 13:35:17 +0800

    leetcode

---
+++ src/leetcode/Solution1646.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+public class Solution1646 {
+    public int getMaximumGenerated(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int[] nums = new int[n + 1];
+        nums[0] = 0;
+        nums[1] = 1;
+        int max = nums[1];
+        for (int i = 2; i <= n; i++) {
+            if (i % 2 == 0) {
+                nums[i] = nums[i / 2];
+            } else {
+                nums[i] = nums[i / 2] + nums[i / 2 + 1];
+            }
+            if (max < nums[i]) {
+                max = nums[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Solution1646 s = new Solution1646();
+        System.out.println(s.getMaximumGenerated(3));
+    }
+}
