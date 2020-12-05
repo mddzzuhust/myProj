2dff656f772234b14b735a91cf27bb873883b9ff
mddzzuhust <1539924369@qq.com>
2020-12-05 22:06:16 +0800

    leetcode

---
+++ src/leetcode/Solution1531.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class Solution1531 {
+    public int getLengthOfOptimalCompression(String s, int k) {
+        int n = s.length();
+        int[][] dp = new int[n + 1][k + 1];
+        for (int[] x : dp) {
+            Arrays.fill(x, n + 1);
+        }
+        dp[0][0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= i && j <= k; j++) {
+                if (j < k) {
+                    dp[i][j + 1] = Math.min(dp[i - 1][j], dp[i][j + 1]);
+                }
+                int del = 0;
+                for (int l = i; l > 0; l--) {
+                    if(s.charAt(l-1)!=s.charAt(i-1)){
+                        if(++del>j){
+                            break;
+                        }
+                    }else {
+                        dp[i][j] = Math.min(dp[l-1][j-del]+len(i-l+1-del)+1,dp[i][j]);
+                    }
+                }
+            }
+        }
+        return dp[n][k];
+    }
+    public int len(int k){
+        if(k<=1) return 0;
+        else if(k>1&&k<10) return 1;
+        else if(k>=10&&k<100) return 2;
+        else return 3;
+    }
+
+    public static void main(String[] args) {
+        Solution1531 s = new Solution1531();
+        System.out.println(s.getLengthOfOptimalCompression("aabbaa",2));
+    }
+}
