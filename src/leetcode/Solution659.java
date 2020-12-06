811348951a21021eba145e332423a4e5aa78b90d
mddzzuhust <1539924369@qq.com>
2020-12-06 20:23:13 +0800

    leetcode

---
+++ src/leetcode/Solution659.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+public class Solution659 {
+    public boolean isPossible(int[] nums) {
+        Map<Integer, PriorityQueue<Integer>> map = new HashMap<Integer, PriorityQueue<Integer>>();
+        for (int x : nums) {
+            if (!map.containsKey(x)) {
+                map.put(x, new PriorityQueue<Integer>());
+            }
+            if (map.containsKey(x - 1)) {
+                int prevLength = map.get(x - 1).poll();
+                if (map.get(x - 1).isEmpty()) {
+                    map.remove(x - 1);
+                }
+                map.get(x).offer(prevLength + 1);
+            } else {
+                map.get(x).offer(1);
+            }
+        }
+        Set<Map.Entry<Integer, PriorityQueue<Integer>>> entrySet = map.entrySet();
+        for (Map.Entry<Integer, PriorityQueue<Integer>> entry : entrySet) {
+            PriorityQueue<Integer> queue = entry.getValue();
+            if (queue.peek() < 3) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution659 s = new Solution659();
+        int[] nums = new int[]{1,2,3,4,4,5};
+        System.out.println(s.isPossible(nums));
+    }
+}
