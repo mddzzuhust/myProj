619a50f9641c17f2e66122a558f06e793d5d3cc9
mddzzuhust <1539924369@qq.com>
2020-12-12 16:45:07 +0800

    leetcode

---
+++ src/leetcode/Solution865.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+public class Solution865 {
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return dfs(root).node;
+    }
+
+    public Result dfs(TreeNode node) {
+        if (node == null) {
+            return new Result(null, 0);
+        }
+        Result L = dfs(node.left),
+                R = dfs(node.right);
+        if (L.dist > R.dist)
+            return new Result(L.node, L.dist + 1);
+        if (L.dist < R.dist)
+            return new Result(R.node, R.dist + 1);
+        return new Result(node, L.dist + 1);
+    }
+
+    class Result {
+        TreeNode node;
+        int dist;
+
+        Result(TreeNode n, int d) {
+            node = n;
+            dist = d;
+        }
+    }
+}
