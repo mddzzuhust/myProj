e9dc520fbf978a3ce62f5ae2dae506525eaec8b1
mddzzuhust <1539924369@qq.com>
2020-12-12 12:57:15 +0800

    leetcode

---
+++ src/leetcode/Solution1535.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class Solution1535 {
+    public int getWinner(int[] arr, int k) {
+        int n = arr.length;
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            if (arr[0] > arr[i]){
+                count++;
+            }else {
+                arr[0] = arr[i];
+                count=1;
+            }
+            if(count == k){
+                return arr[0];
+            }
+        }
+        return arr[0];
+    }
+
+    public static void main(String[] args) {
+        Solution1535 s = new Solution1535();
+        int[] nums = new int[]{1, 11, 22, 33, 44, 55, 66, 77, 88, 99};
+        System.out.println(s.getWinner(nums, 100000));
+    }
+}
