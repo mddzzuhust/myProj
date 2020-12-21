0667171709bca838b486ce9c4ee9c46b6883548f
mddzzuhust <1539924369@qq.com>
2020-12-21 14:20:08 +0800

    leetcode

---
+++ src/leetcode/Solution1695.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import Greedy.Solution;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution1695 {
+    public int maximumUniqueSubarray(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int max = 0, sum = 0, start = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(!set.contains(nums[i])){
+                set.add(nums[i]);
+                sum += nums[i];
+                max = Math.max(sum,max);
+            }else {
+                while (nums[i]!=nums[start]){
+                    sum -= nums[start];
+                    set.remove(nums[start]);
+                    start++;
+                }
+                start++;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Solution1695 s = new Solution1695();
+        int[] nums = new int[]{5,2,1,2,5,2,1,2,5};
+        System.out.println(s.maximumUniqueSubarray(nums));
+    }
+}
---
+++ src/leetcode/Solution1696.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import Greedy.Solution;
+
+public class Solution1696 {
+    public int maxResult(int[] nums, int k) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int maxIndex = 0;
+        for (int i = 1; i < nums.length; i++) {
+            int max = Integer.MIN_VALUE;
+            if (maxIndex >= i - k) {
+                max = dp[maxIndex];
+            } else {
+                for (int j = i - 1; j >= 0 && j >= i - k; j--) {
+                    max = Math.max(max, dp[j]);
+                }
+            }
+            dp[i] = max + nums[i];
+            if (dp[i] > dp[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        return dp[nums.length - 1];
+    }
+
+    public static void main(String[] args) {
+        Solution1696 s = new Solution1696();
+        int[] nums = new int[]{1,-5,-20,4,-1,3,-6,-3};
+        System.out.println(s.maxResult(nums,2));
+    }
+}
