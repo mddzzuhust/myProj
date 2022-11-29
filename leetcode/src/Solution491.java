89733b86c1a682238ee6a5ed39f05c2cf3cca77a
mddzzuhust <1539924369@qq.com>
2022-11-29 23:26:53 +0800

    leetcode

---
+++ leetcode/src/Solution491.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution491 {
+    List<Integer> temp = new ArrayList<>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        dfs(0,Integer.MIN_VALUE,nums);
+        return ans;
+    }
+
+    public void dfs(int cur, int last, int[] nums){
+        if(cur == nums.length){
+            if(temp.size() >= 2){
+                ans.add(new ArrayList<Integer>(temp));
+            }
+            return;
+        }
+
+        if(nums[cur] >= last){
+            temp.add(nums[cur]);
+            dfs(cur+1, nums[cur],nums);
+            temp.remove(temp.size() - 1);
+        }
+
+        if(nums[cur] != last){
+            dfs(cur+1, last, nums);
+        }
+    }
+}
