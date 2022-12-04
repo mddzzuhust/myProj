ac78de3b6bd4bd61d9d83e9468375159fb658649
mddzzuhust <1539924369@qq.com>
2022-12-04 22:15:21 +0800

    leetcode

---
+++ leetcode/src/Solution592.java
@@ -0,0 +1,44 @@
+public class Solution592 {
+    public String fractionAddition(String expression) {
+        long x = 0,y=1;
+        int index = 0,n = expression.length();
+        while (index < n){
+            long x1 = 0,sign = 1;
+            if(expression.charAt(index) == '-' || expression.charAt(index) == '+'){
+                sign = expression.charAt(index) == '-' ? -1 : 1;
+                index++;
+            }
+
+            while (index < n && Character.isDigit(expression.charAt(index))){
+                x1 = x1 * 10 + expression.charAt(index) - '0';
+                index++;
+            }
+            x1 = sign * x1;
+            index++;
+
+            long y1 = 0;
+            while (index < n && Character.isDigit(expression.charAt(index))){
+                y1 = y1 * 10 + expression.charAt(index) - '0';
+                index++;
+            }
+
+            x = x*y1 + x1*y;
+            y *= y1;
+        }
+        if(x==0){
+            return "0/1";
+        }
+        long g = gcd(Math.abs(x),y);
+        return Long.toString(x/g) + "/" + Long.toString(y/g);
+    }
+
+    public long gcd(long a, long b){
+        long remainder = a%b;
+        while (remainder != 0 ){
+            a = b;
+            b = remainder;
+            remainder = a%b;
+        }
+        return b;
+    }
+}
