f0e16911ce24aaefaf5ebaf88ce5eb1f53ed297a
mddzzuhust <1539924369@qq.com>
2022-12-10 11:26:48 +0800

    leetcode

---
+++ leetcode/src/Solution524.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class Solution524 {
+    public String findLongestWord(String s, List<String> dictionary) {
+        String res = "";
+        for(String t : dictionary){
+            int i = 0, j = 0;
+            while (i < t.length() && j < s.length()){
+                if(t.charAt(i) == s.charAt(j)){
+                    i++;
+                }
+                j++;
+            }
+            if(i == t.length()){
+                if(t.length() > res.length() || (t.length() == res.length() && t.compareTo(res) < 0)){
+                    res = t;
+                }
+            }
+        }
+        return res;
+    }
+}
