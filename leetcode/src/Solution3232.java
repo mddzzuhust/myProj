e30ba9a9a88ec2a4d027e979698a30aff44c136e
mddzzuhust <1539924369@qq.com>
2024-12-08 14:50:59 +0800

    优化文档

---
+++ leetcode/src/Solution3232.java
@@ -0,0 +1,14 @@
+public class Solution3232 {
+    public boolean canAliceWin(int[] nums) {
+        int singleDigitSum = 0;
+        int doubleDigitSum = 0;
+        for(int num : nums){
+            if(num < 10){
+                singleDigitSum += num;
+            }else {
+                doubleDigitSum += num;
+            }
+        }
+        return singleDigitSum != doubleDigitSum;
+    }
+}
