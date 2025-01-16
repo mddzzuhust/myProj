11eaffbca7d8cf81273f5f8e2c8bf23b53fd0f85
mddzzuhust <1539924369@qq.com>
2025-01-16 12:38:11 +0800

    数据结构

---
+++ dataStructure/src/tree/Trie.java
@@ -0,0 +1,53 @@
+package tree;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class TrieNode {
+    boolean isWord;
+    Map<Character, TrieNode> children;
+
+    TrieNode() {
+        isWord = false;
+        children = new HashMap<>();
+    }
+}
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (!node.children.containsKey(c)) {
+                node.children.put(c, new TrieNode());
+            }
+            node = node.children.get(c);
+        }
+        node.isWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        return searchPrefix(prefix) != null;
+    }
+
+    private TrieNode searchPrefix(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            if (!node.children.containsKey(c)) {
+                return null;
+            }
+            node = node.children.get(c);
+        }
+        return node;
+    }
+}
