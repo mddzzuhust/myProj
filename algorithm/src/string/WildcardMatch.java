aaec708c6722b1662ec4146a3559b79832b88446
mddzzuhust <1539924369@qq.com>
2025-01-16 22:42:07 +0800

    WildcardMatch

--- algorithm/src/string/WildcardMatch.java
+++ algorithm/src/string/WildcardMatch.java
@@ -1,5 +1,11 @@
 package string;
-
+/*
+通配符匹配:
+给你一个输入字符串 (s) 和一个字符模式 (p) ，请你实现一个支持 '?' 和 '*' 匹配规则的通配符匹配：
+'?' 可以匹配任何单个字符。
+'*' 可以匹配任意字符序列（包括空字符序列）。
+判定匹配成功的充要条件是：字符模式必须能够 完全匹配 输入字符串（而不是部分匹配）。
+ */
 public class WildcardMatch {
     public boolean isMatch(String s, String p) {
         p=prepare(p);
